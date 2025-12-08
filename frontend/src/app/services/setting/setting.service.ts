0e455949d6b42ed8a5cbec95b0b93df042433d66
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-12-08 03:21:49 -0300

    add: local storage for theme

--- frontend/src/app/services/setting/setting.service.ts
+++ frontend/src/app/services/setting/setting.service.ts
@@ -1,21 +1,30 @@
 import {Injectable, signal} from '@angular/core';
 import {ThemeType} from '../../helper/helper.viewer';
+import {HelperStorage} from '../../helper/helper.storage';
 
 @Injectable({providedIn: 'root'})
 export class SettingsService {
-	isSettingsVisible = signal<boolean>(false);
-	theme = signal<ThemeType>('dark');
-	isDiscordRpcEnabled = signal<boolean>(true);
 
-	toggleSettings(visible: boolean): void {
+	public readonly theme = signal<ThemeType>('dark');
+	public readonly isSettingsVisible = signal<boolean>(false);
+	public readonly isDiscordRpcEnabled = signal<boolean>(true);
+
+	constructor() {
+		this.theme.set(<"light" | "dark">localStorage.getItem(HelperStorage.THEME) ?? 'dark')
+	}
+
+	public toggleTheme(): void {
+		this.theme.update(current => current === 'dark' ? 'light' : 'dark');
+
+		localStorage.setItem(HelperStorage.THEME, this.theme())
+	}
+
+	public toggleSettings(visible: boolean): void {
 		this.isSettingsVisible.set(visible);
 	}
 
-	toggleTheme(): void {
-		this.theme.update(current => current === 'dark' ? 'light' : 'dark');
-	}
-
-	toggleDiscordRpc(): void {
+	public toggleDiscordRpc(): void {
 		this.isDiscordRpcEnabled.update(enabled => !enabled);
 	}
+
 }
