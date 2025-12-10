3e1938abae617f125b4a69f348091bc077471096
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-12-10 00:04:07 -0300

    chore: environment

--- frontend/src/environments/environment.electron.ts
+++ frontend/src/environments/environment.electron.ts
@@ -3,7 +3,7 @@ import {Environment} from './models/environment.model';
 export const environment: Environment = {
 	name: 'Electron Launcher',
 
-	apiUrl: 'http://localhost:8080',
+	apiUrl: 'https://api-launcher.anthhyo.dev',
 
 	backgroundImageUrl: 'https://picsum.photos/seed/cyberpunk/1920/1080',
 	loadingImageUrl: 'https://picsum.photos/seed/launcher-art/512/288',
