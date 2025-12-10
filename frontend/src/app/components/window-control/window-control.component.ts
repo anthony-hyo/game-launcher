2b7a884d348c1b157c6b22b98f6f9c9527a32f94
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-12-09 23:17:43 -0300

    improve: window control using electron api

--- frontend/src/app/components/window-control/window-control.component.ts
+++ frontend/src/app/components/window-control/window-control.component.ts
@@ -1,7 +1,7 @@
-import {Component, inject, input, signal} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
+import {Component, inject, input} from '@angular/core';
 import {StateService} from '../../services/state/state.service';
 import {environment} from '../../../environments/environment';
+
 
 @Component({
 	selector: 'app-window-control',
@@ -13,36 +13,22 @@ export class WindowControll {
 
 	protected readonly environment = environment;
 
-	private readonly document = inject(DOCUMENT);
-
 	protected readonly state = inject(StateService);
-	protected readonly isFullscreen = signal(false);
 
 	public readonly hasMinimize = input<boolean>(true);
 	public readonly hasMaximize = input<boolean>(true);
 	public readonly hasClose = input<boolean>(true);
 
 	protected minimize(): void {
-		console.log('Application minimized to tray');
+		window.electron.window_minimize()
 	}
 
 	protected toggleMaximize(): void {
-		if (!this.document.fullscreenElement) {
-			this.document.documentElement.requestFullscreen().then(() => {
-				this.isFullscreen.set(true);
-			});
-			return
-		}
-
-		if (this.document.exitFullscreen) {
-			this.document.exitFullscreen().then(() => {
-				this.isFullscreen.set(false);
-			});
-		}
+		window.electron.window_maximize()
 	}
 
 	protected close(): void {
-		window.close();
+		window.electron.window_close();
 	}
 
 }
