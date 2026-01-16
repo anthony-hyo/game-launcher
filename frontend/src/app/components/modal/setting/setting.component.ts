286c179cdf3c6772445c5311ef31467439d32920
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2026-01-16 11:07:15 -0300

    fix: disable rpc input if web view is not being used

--- frontend/src/app/components/modal/setting/setting.component.ts
+++ frontend/src/app/components/modal/setting/setting.component.ts
@@ -3,6 +3,7 @@ import {SettingService} from '../../../services/setting/setting.service';
 import {StateService} from '../../../services/state/state.service';
 import {ModalType} from '../../../constants/modal.const';
 import {Modal} from '../modal.component';
+import {environment} from '../../../../environments/environment';
 
 @Component({
 	selector: 'app-setting',
@@ -14,6 +15,7 @@ import {Modal} from '../modal.component';
 })
 export class Setting {
 
+	protected readonly environment = environment;
 	protected readonly ModalType = ModalType;
 
 	protected readonly stateService = inject(StateService);
